8eda5ccad9ee9840271859bf95cdbd44e35b9715
tastypeanut <tastypeanut@users.noreply.github.com>
2020-12-22 02:51:57 +0100

    Finished file ingress

--- main.py
+++ main.py
@@ -19,33 +19,38 @@ def main():
 
     filelinelist = fileraw.read().split('\n')
 
-    observationlist = []
-    satellitelist = []
+    observationliststart = []
+    satelliteliststart = []
 
     try:
         idcounter = 0
 
         for obscoord in re.sub('([OBS: ]|[()])', '', filelinelist[0]).split(';'):
             idcounter += 1
-            obscoordlist = obscoord.split(',')
-            observationlist.append(Observation.observation(idcounter, obscoordlist[0], obscoordlist[1]))
+            obscoordlist = list(map(int, obscoord.split(',')))
+            observationliststart.append(Observation.observation(idcounter, obscoordlist[0], obscoordlist[1]))
             #print("obs", idcounter, obscoordlist[0], obscoordlist[1])
+            
         idcounter = 0
 
         for position in range(1, len(filelinelist)):
             idcounter += 1
-            poscoordlist = re.sub('(SAT)\w+: {1}', '', filelinelist[position]).split(';')
-            satellitelist.append(Satellite.satellite(idcounter, (idcounter), 0, "iddle", poscoordlist))
+            poscoordlist = list(map(int, re.sub('(SAT)\w+: {1}', '', filelinelist[position]).split(';')))
+            satelliteliststart.append(Satellite.satellite(idcounter, (idcounter), 0, "iddle", poscoordlist))
             #print("pos", idcounter, (idcounter), 0, "iddle", poscoordlist)
+
     except:
         print(Back.RED + " Object creation failed: Invalid file contents " + Style.RESET_ALL)
         exit(1)
     
+    satellitelistgoal = []
+    observationlistgoal = []
 
-    InitialNode = Node.node(None, satellitelist, observationlist)
-    FinalNode = Node.node(None, None, None)
+    InitialNode = Node.node(None, satelliteliststart, observationliststart)
+    FinalNode = Node.node(None, satellitelistgoal, observationlistgoal)
     astar = AStar.astar(InitialNode, FinalNode)
     astar.algorithm()
+
     fileraw.close()
 
 
